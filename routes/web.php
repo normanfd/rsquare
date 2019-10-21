80e2cc345f0e5f0cba236213672d7b93f9115514
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-10-21 21:10:22 +0700

    tampilan login

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('admin')->group(function() {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/login2', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login2', 'Auth\AdminLoginController@login')->name('admin.login.submit');
     Route::get('/', 'AdminController@index')->name('admin.dashboard');
     Route::get('/VWAvailableProduct', 'AvailableProductController@VWAvailableProduct')->name('admin.VWAvailProduct');
   });
