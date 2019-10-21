010923e01f2f8b03d9df7e4adda9ba0295ba6b27
normanfd <norman_fd@apps.ipb.ac.id>
2019-10-21 19:03:04 +0700

    VW Form Available product

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::prefix('admin')->group(function() {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
     Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/VWAvailableProduct', 'AvailableProductController@VWAvailableProduct')->name('admin.VWAvailProduct');
   });
