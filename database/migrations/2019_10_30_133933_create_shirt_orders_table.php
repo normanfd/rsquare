1e0897111ad78114c5887c9d229514c17f1afefa
normanfd <norman_fd@apps.ipb.ac.id>
2019-10-30 21:26:18 +0700

    add model and migration order custom product

---
+++ database/migrations/2019_10_30_133933_create_shirt_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShirtOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shirt_orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('shirt_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('material');
+            $table->string('color');
+            $table->string('collar');
+            $table->string('sleeve');
+            $table->string('sleeve_model');
+            $table->string('pocket');
+            $table->string('buttons');
+            $table->integer('size');
+            $table->string('upload_design');
+            $table->integer('amount');
+            $table->string('wa_number');
+            $table->string('note');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
+            $table->foreign('shirt_id')->references('id')->on('shirts')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shirt_orders');
+    }
+}
