88fd7085a40482adae8bbb9dcc2d4bb424d0764a
bayuadam13 <d.bayuadam13@gmail.com>
2019-11-06 19:16:20 +0700

    tampilan logiin terbaru

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,193 +1,75 @@
-<!DOCTYPE html>
-<!-- 
-Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
-<html lang="en" >
-	<!-- begin::Head -->
-	<head>
-		<meta charset="utf-8" />
-		<title>
-			Metronic | Login Page - 2
-		</title>
-		<meta name="description" content="Latest updates and statistic charts">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!--begin::Web font -->
-		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
-		<script>
-          WebFont.load({
-            google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
-            active: function() {
-                sessionStorage.fonts = true;
-            }
-          });
-		</script>
-		<!--end::Web font -->
-        <!--begin::Base Styles -->
-		<link href="../../../assets/vendors/base/vendors.bundle.css" rel="stylesheet" type="text/css" />
-		<link href="../../../assets/demo/default/base/style.bundle.css" rel="stylesheet" type="text/css" />
-		<!--end::Base Styles -->
-		<link rel="shortcut icon" href="../../../assets/demo/default/media/img/logo/favicon.ico" />
-	</head>
-	<!-- end::Head -->
-    <!-- end::Body -->
-	<body  class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default"  >
-		<!-- begin:: Page -->
-		<div class="m-grid m-grid--hor m-grid--root m-page">
-			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-1" id="m_login" style="background-image: url(../../../assets/app/media/img//bg/bg-1.jpg);">
-				<div class="m-grid__item m-grid__item--fluid m-login__wrapper">
-					<div class="m-login__container">
-						<div class="m-login__logo">
-							<a href="#">
-								<img src="../../../assets/app/media/img//logos/logo-1.png">
-							</a>
-						</div>
-						<div class="m-login__signin">
-							<div class="m-login__head">
-								<h3 class="m-login__title">
-									Sign In To Admin
-								</h3>
-							</div>
-							<form class="m-login__form m-form" method="POST" action="{{ route('login') }}">
-                            {{ csrf_field() }}
-								<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} m-form__group">
-									<input id="email" class="form-control m-input"   type="email" placeholder="Email" value="{{ old('email') }}" name="email">
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                                    @endif
-								</div>
-								<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} m-form__group">
-									<input id="password" class="form-control m-input m-login__form-input--last" type="password" placeholder="Password" name="password">
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
-								</div>
-								<div class="row m-login__form-sub">
-									<div class="col m--align-left m-login__form-left">
-										<label class="m-checkbox  m-checkbox--light">
-											<input type="checkbox" name="remember">
-											Remember me
-											<span></span>
-										</label>
-									</div>
-									<div class="col m--align-right m-login__form-right">
-										<a href="javascript:;" id="m_login_forget_password" class="m-link">
-											Forget Password ?
-										</a>
-									</div>
-								</div>
-								<div class="m-login__form-action">
-									<button id="m_login_signin_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primary">
-										Sign In
-									</button>
-								</div>
-							</form>
-						</div>
-						<div class="m-login__signup">
-							<div class="m-login__head">
-								<h3 class="m-login__title">
-									Sign Up
-								</h3>
-								<div class="m-login__desc">
-									Enter your details to create your account:
-								</div>
-							</div>
-							<form class="m-login__form m-form" action="">
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="text" placeholder="Fullname" name="fullname">
-								</div>
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="text" placeholder="Email" name="email" autocomplete="off">
-								</div>
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="password" placeholder="Password" name="password">
-								</div>
-								<div class="form-group m-form__group">
-									<input class="form-control m-input m-login__form-input--last" type="password" placeholder="Confirm Password" name="rpassword">
-								</div>
-								<div class="row form-group m-form__group m-login__form-sub">
-									<div class="col m--align-left">
-										<label class="m-checkbox m-checkbox--light">
-											<input type="checkbox" name="agree">
-											I Agree the
-											<a href="#" class="m-link m-link--focus">
-												terms and conditions
-											</a>
-											.
-											<span></span>
-										</label>
-										<span class="m-form__help"></span>
-									</div>
-								</div>
-								<div class="m-login__form-action">
-									<button id="m_login_signup_submit" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary">
-										Sign Up
-									</button>
-									&nbsp;&nbsp;
-									<button id="m_login_signup_cancel" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn">
-										Cancel
-									</button>
-								</div>
-							</form>
-						</div>
-						<div class="m-login__forget-password">
-							<div class="m-login__head">
-								<h3 class="m-login__title">
-									Forgotten Password ?
-								</h3>
-								<div class="m-login__desc">
-									Enter your email to reset your password:
-								</div>
-							</div>
-							<form class="m-login__form m-form" action="">
-								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="text" placeholder="Email" name="email" id="m_email" autocomplete="off">
-								</div>
-								<div class="m-login__form-action">
-									<button id="m_login_forget_password_submit" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary">
-										Request
-									</button>
-									&nbsp;&nbsp;
-									<button id="m_login_forget_password_cancel" class="btn m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn">
-										Cancel
-									</button>
-								</div>
-							</form>
-						</div>
-						<div class="m-login__account">
-							<span class="m-login__account-msg">
-								Don't have an account yet ?
-							</span>
-							&nbsp;&nbsp;
-							<a href="javascript:;" id="m_login_signup" class="m-link m-link--light m-login__account-link">
-								Sign Up
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- end:: Page -->
-    	<!--begin::Base Scripts -->
-		<script src="../../../assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
-		<script src="../../../assets/demo/default/base/scripts.bundle.js" type="text/javascript"></script>
-		<!--end::Base Scripts -->   
-        <!--begin::Page Snippets -->
-		<!-- <script src="../../../assets/snippets/custom/pages/user/login.js" type="text/javascript"></script> -->
-		<!--end::Page Snippets -->
-	</body>
-	<!-- end::Body -->
-</html>
+   <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>winter</title>
+    <link rel="icon" href="img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/bootstrap.min.css')}}">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/animate.css')}}">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/owl.carousel.min.css')}}">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/all.css')}}">
+    <link rel="stylesheet" href="{{asset('winter/css/nice-select.css')}}">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/flaticon.css')">
+    <link rel="stylesheet" href="{{asset('winter/css/themify-icons.css')}}">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/magnific-popup.css')}}">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/slick.css')}}">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/price_rangs.css')}}">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="{{asset('winter/css/style.css')}}">
+</head>
+<body>
+    <!--================login_part Area =================-->
+    <section class="login_part section_padding">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6">
+                    <div class="login_part_text text-center">
+                        <div class="login_part_text_iner">
+                            <h2>New to our Shop?</h2>
+                            <p>There are advances being made in science and technology
+                                everyday, and a good example of this is the</p>
+                            <a href="#" class="btn_3">Create an Account</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="login_part_form">
+                        <div class="login_part_form_iner">
+                            <h3>Welcome Back ! <br>
+                                Please Sign in now</h3>
+                            <form class="row contact_form" action="#" method="post" novalidate="novalidate">
+                                <div class="col-md-12 form-group p_star">
+                                    <input type="text" class="form-control" id="name" name="name" value=""
+                                        placeholder="Username">
+                                </div>
+                                <div class="col-md-12 form-group p_star">
+                                    <input type="password" class="form-control" id="password" name="password" value=""
+                                        placeholder="Password">
+                                </div>
+                                <div class="col-md-12 form-group">
+                                    <div class="creat_account d-flex align-items-center">
+                                        <input type="checkbox" id="f-option" name="selector">
+                                        <label for="f-option">Remember me</label>
+                                    </div>
+                                    <button type="submit" value="submit" class="btn_3">
+                                        log in
+                                    </button>
+                                    <a class="lost_pass" href="#">forget password?</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================login_part end =================-->
+</body>
