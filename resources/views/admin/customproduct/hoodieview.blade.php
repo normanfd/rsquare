b99752c51ad728a9c3cd9e8d15c12a6505412734
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-11-09 20:16:08 +0700

    edit custom product view

--- resources/views/admin/customproduct/hoodieview.blade.php
+++ resources/views/admin/customproduct/hoodieview.blade.php
@@ -1,14 +1,61 @@
-@foreach($hoodie as $produk)
-    {{ $produk->id}}
-    {{ $produk->hoodie_name}}
-    <a href="{{ route('admin.Editcustomhoodie', $produk->id) }}" type="button">Edit</a>
-    <div class="m-widget4__ext">
-        <!-- {{ route('admin.DeleteAvailProduct', $produk->id) }} -->
-        <form action="{{ route('admin.Deletecustomhoodie', $produk->id) }}" method="post">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
-            <button class="m-btn  btn btn-sm btn-danger" type="submit">Delete</button>
-        </form>
+<!-- Mendapatkan data available product -->
+@extends('layouts.admin')
+
+@section('content')
+<div class="m-content">
+
+    <!--begin:: Widgets/New Users-->
+    <div class="m-portlet m-portlet--full-height ">
+        <div class="m-portlet__head">
+            <div class="m-portlet__head-caption">
+                <div class="m-portlet__head-title">
+                    <h3 class="m-portlet__head-text">
+                        View Hoodie Product
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <div class="m-portlet__body">
+            <div class="tab-content">
+                <div class="tab-pane active" id="m_widget4_tab1_content">
+
+                    <!--begin::Widget 14-->
+                    @foreach($hoodie as $produk)
+                    <div class="m-widget4">
+                        <div class="m-widget4__item">
+                            <div class="m-widget4__img m-widget4__img--pic">
+                                <img src="../storage/{{ $produk->hoodie_image }}" alt="" width="50" height="50">
+                            </div>
+                            <div class="m-widget4__info">
+                                <span class="m-widget4__title">
+                                    {{ $produk->hoodie_name }}
+                                </span>
+                                <br>
+                                <span class="m-widget4__sub">
+                                    harga produk : {{ $produk->hoodie_price }}
+                                </span>
+                            </div>
+                            <div class="m-widget4__ext">
+                                <a href="{{ route('admin.Editcustomhoodie', $produk->id)}}" class="m-btn m-btn--hover-brand btn btn-sm btn-primary">Edit</a>
+                            </div>
+                            <div class="m-widget4__ext">
+                                <form action="{{ route('admin.Deletecustomhoodie', $produk->id) }}" method="post">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <button class="m-btn  btn btn-sm btn-danger" type="submit">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
-    <br>
-@endforeach
+</div>
+    
+    
+    
+    
+
+@endsection
--- resources/views/admin/customproduct/shirtview.blade.php
+++ resources/views/admin/customproduct/shirtview.blade.php
@@ -1,14 +1,62 @@
-@foreach($shirt as $produk)
-    {{ $produk->id}}
-    {{ $produk->shirt_name}}
-    <a href="{{ route('admin.Editcustomshirt', $produk->id) }}" type="button">Edit</a>
-    <div class="m-widget4__ext">
-        <!-- {{ route('admin.DeleteAvailProduct', $produk->id) }} -->
-        <form action="{{ route('admin.Deletecustomshirt', $produk->id) }}" method="post">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
-            <button class="m-btn  btn btn-sm btn-danger" type="submit">Delete</button>
-        </form>
+
+<!-- Mendapatkan data available product -->
+@extends('layouts.admin')
+
+@section('content')
+<div class="m-content">
+
+    <!--begin:: Widgets/New Users-->
+    <div class="m-portlet m-portlet--full-height ">
+        <div class="m-portlet__head">
+            <div class="m-portlet__head-caption">
+                <div class="m-portlet__head-title">
+                    <h3 class="m-portlet__head-text">
+                        View Shirt Product
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <div class="m-portlet__body">
+            <div class="tab-content">
+                <div class="tab-pane active" id="m_widget4_tab1_content">
+
+                    <!--begin::Widget 14-->
+                    @foreach($shirt as $produk)
+                    <div class="m-widget4">
+                        <div class="m-widget4__item">
+                            <div class="m-widget4__img m-widget4__img--pic">
+                                <img src="../storage/{{ $produk->shirt_image }}" alt="" width="50" height="50">
+                            </div>
+                            <div class="m-widget4__info">
+                                <span class="m-widget4__title">
+                                    {{ $produk->shirt_name }}
+                                </span>
+                                <br>
+                                <span class="m-widget4__sub">
+                                    harga produk : {{ $produk->shirt_price }}
+                                </span>
+                            </div>
+                            <div class="m-widget4__ext">
+                                <a href="{{ route('admin.Editcustomshirt', $produk->id)}}" class="m-btn m-btn--hover-brand btn btn-sm btn-primary">Edit</a>
+                            </div>
+                            <div class="m-widget4__ext">
+                                <form action="{{ route('admin.Deletecustomshirt', $produk->id) }}" method="post">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <button class="m-btn  btn btn-sm btn-danger" type="submit">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
-    <br>
-@endforeach
+</div>
+    
+    
+    
+    
+
+@endsection
