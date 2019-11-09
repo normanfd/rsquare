d0a951dad143b6a167df02bafb2dc0a423e2e200
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-11-09 19:32:40 +0700

    alert

--- resources/views/layouts/partials/alert.blade.php
+++ resources/views/layouts/partials/alert.blade.php
@@ -1,17 +1,20 @@
 @if(session('success'))
-    <div class="alert alerts-success">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
         {{ session('success')}}
     </div> 
 @endif
 
 @if(session('info'))
-    <div class="alert alerts-info">
+    <div class="alert alert-info alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
         {{ session('info')}}
     </div>
 @endif
 
 @if(session('danger'))
-    <div class="alert alerts-danger">
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
         {{ session('danger')}}
     </div>
 @endif
